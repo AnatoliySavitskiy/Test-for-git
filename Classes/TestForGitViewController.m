3229955912bd0f829a8765e3c7f5fef7c0f01e08
Anatoliy.Savitskiy <anatoliysavitskiy@mac3.local>
2011-03-09 14:15:07 +0200

    Change background

--- Classes/TestForGitViewController.m
+++ Classes/TestForGitViewController.m
@@ -29,12 +29,13 @@
 */
 
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
-    [super viewDidLoad];
+	[super viewDidLoad];
+	self.view.backgroundColor = [UIColor grayColor];
 }
-*/
+
 
 
 // Override to allow orientations other than the default portrait orientation.
